58bc4a464b03c179ae00a1932fa0f3f1b7a76e2e
KuroSol <cyrusinout@gmail.com>
2024-08-26 22:06:09 +0900

    Removed all files

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-# gunicorn_config.py
-pythonpath = '/home/cyrus/eduhab-backend'
-bind = 'unix:/home/cyrus/eduhab-backend/eduhab_backend.sock'
-workers = 3
-timeout = 120
-loglevel = 'debug'
-errorlog = '/home/cyrus/logs/gunicorn_error.log'
-accesslog = '/home/cyrus/logs/gunicorn_access.log'
